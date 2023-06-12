60fc92b887080c52da0dc6c53c374c5aee75d8fa
Sherly1001 <Sherly1001@users.noreply.github.com>
2023-06-13 01:07:00 +0700

    popup: init chakra ui

--- popup/src/main.tsx
+++ popup/src/main.tsx
@@ -1,9 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
---
+++ popup/src/theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export default extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+});
