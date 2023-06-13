04f63b57114540922f35602c5ba650678df27954
Sherly1001 <Sherly1001@users.noreply.github.com>
2023-06-13 12:45:58 +0700

    popup: vite split vendor chunk

--- popup/vite.config.ts
+++ popup/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   base: "/popup/dist",
   server: {
     fs: {
